aab8c406f5fb9873909520760c2591196c4e78bc
AtifAwan-Teratone <atif@teratone.com>
2022-07-27 14:01:32 +0500

    New changes

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
   /*This is php file.*/
-  echo 'Git Demo';
-  echo 'Git Demo 2';
-  echo '123456';
-<<<<<<< HEAD
-  echo 'ABCD';
-=======
-  echo 'Test 123';
->>>>>>> origin/main
+	echo 'Git Demo';
+	echo 'Git Demo 2';
+	echo '123456';
+	echo 'ABCD';
+	echo 'Test 123';
 ?>
